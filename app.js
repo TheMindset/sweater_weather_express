5df1b240816e8a0054616f132dc5c4fd5b187e35
TheMindset <mulezig2014@outlook.fr>
2019-12-13 09:16:31 +0100

    fix routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes//api/v1/users/users');
+const usersRouter = require('./routes/api/v1/users/users');
 
 const app = express();
 
