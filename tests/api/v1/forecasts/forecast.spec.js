7d1aa30614079b97d84e1715b18bc8fb9340a86a
TheMindset <mulezig2014@outlook.fr>
2019-12-15 19:53:21 +0100

    Refactor forecasts spec

--- tests/api/v1/forecasts/forecast.spec.js
+++ tests/api/v1/forecasts/forecast.spec.js
@@ -15,7 +15,7 @@ describe('forecast endpoint', () => {
     
   })
 
-  afterAlL(() => {
+  afterAll(() => {
     shell.exec('npx sequelize db:undo:all')
   })
 
@@ -28,13 +28,20 @@ describe('forecast endpoint', () => {
     })
     .then(user => {
       request(app)
-      .get('/api/v1/forecast?location=paris')
+      .get('/api/v1/forecasts?location=Paris')
       .send({
         api_key: user.api_key
       })
       .then(response => {
         expect(response.statusCode).toBe(200)
         expect(Object.keys(response.body)).toContain('data')
+        expect(Object.keys(response.body.data)).toContain('location');
+        expect(Object.keys(response.body.data)).toContain('currently');
+        expect(Object.keys(response.body.data)).toContain('hourly');
+        expect(Object.keys(response.body.data)).toContain('daily');
+
+        expect(response.body.data.hourly.length).toEqual(8);
+        expect(response.body.data.daily.length).toEqual(7);
       })
     }) 
   })
@@ -42,13 +49,13 @@ describe('forecast endpoint', () => {
 
   test('should returns an error message when wrong api_key is sent ', () => {
     return request(app)
-    .get('/api/v1/forecast?location=paris')
+    .get('/api/v1/forecasts?location=Paris')
     .send({
       api_key: 'MZssD88Ut_C8F_dZxMddsd_AJ8cpYlLcdt'
     })
     .then(response => {
       expect(response.statusCode).toBe(401)
-      expect(Object.keys(response.body)).toContain('Api_key submit is incorrect')
+      expect(response.body.error).toEqual('Api_key submit is incorrect')
     })
   })
 })
