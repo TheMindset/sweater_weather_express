da6850e70d37adb5b37e89994757b1af2be16746
TheMindset <mulezig2014@outlook.fr>
2019-12-17 13:17:12 +0100

    Refactor favorites_locations spec

--- tests/api/v1/favorites_locations/favorites_locations.spec.js
+++ tests/api/v1/favorites_locations/favorites_locations.spec.js
@@ -3,6 +3,7 @@ const shell = require('shelljs')
 const request = require('supertest')
 
 const User = require('../../../../models').User
+const Location = require('../../../../models').Location
 
 describe('favorites locations endpoint', () => {
   beforeAll(() => {
@@ -18,13 +19,13 @@ describe('favorites locations endpoint', () => {
 
 
   test('should persisted a location when valid a valid api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
       api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
     })
     .then(user => {
-      request(app)
+      return request(app)
       .post('/api/v1/favorites?location=Paris')
       .send({
         api_key: user.api_key
@@ -32,50 +33,56 @@ describe('favorites locations endpoint', () => {
       .then(response => {
         expect(response.statusCode).toBe(200)
         expect(Object.keys(response.body)).toContain('message')
-        expect(Object.keys(response.body.message)).toEqual('Paris, has been added to your favorites');
+        expect(response.body.message).toEqual('Paris, has been added to your favorites.');
       })
     }) 
   })
 
   test('should not persisted a location when wrong api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
       api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
     })
     .then(user => {
-      request(app)
+      return request(app)
       .post('/api/v1/favorites?location=Paris')
       .send({
         api_key: 'MZDhxxxt_C8F_dZxMpH_AJ8cpYlLcdt'
       })
       .then(response => {
         expect(response.statusCode).toBe(401)
-        expect(Object.keys(response.body)).toContain('error')
-        expect(Object.keys(response.body.error.message)).toEqual('Api_key submit is incorrect');
+        expect(response.body.error).toEqual('Api_key submit is incorrect');
       })
     }) 
-
   })
 
 
   test('should returns all favorites locations when valid a valid api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
-      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
+      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt',
+      locations: [
+        {
+          city: 'Paris'
+        },
+        {
+          city: 'Kigali'
+        },
+        {
+          city: 'Nairobi'
+        },
+      ]
     })
     .then(user => {
-      Location.create({
-        city: 'Paris',
-        userId: user.id
-      })
-      request(app)
+      return request(app)
       .get('/api/v1/favorites')
       .send({
         api_key: user.api_key
       })
       .then(response => {
+        console.log(response.body)
         expect(response.statusCode).toBe(200)
         expect(Object.keys(response.body.data[0])).toContain('location');
         expect(Object.keys(response.body.data[0].location)).toEqual('Paris');
@@ -85,17 +92,24 @@ describe('favorites locations endpoint', () => {
   })
 
   test('should not returns all favorites locations when a wrong api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
-      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
+      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt',
+      locations: [
+        {
+          city: 'Paris'
+        },
+        {
+          city: 'Kigali'
+        },
+        {
+          city: 'Nairobi'
+        },
+      ]
     })
     .then(user => {
-      Location.create({
-        city: 'Paris',
-        userId: user.id
-      })
-      request(app)
+      return request(app)
       .get('/api/v1/favorites')
       .send({
         api_key: 'MZDhxxxt_C8F_dZxMpH_AJ8cpYlLcdt'
@@ -110,17 +124,24 @@ describe('favorites locations endpoint', () => {
 
 
   test('should delete a favorite location when valid api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
-      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
+      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt',
+      locations: [
+        {
+          city: 'Paris'
+        },
+        {
+          city: 'Kigali'
+        },
+        {
+          city: 'Nairobi'
+        },
+      ]
     })
     .then(user => {
-      Location.create({
-        city: 'Paris',
-        userId: user.id
-      })
-      request(app)
+      return request(app)
       .delete('/api/v1/favorites?location=Paris')
       .send({
         api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
@@ -132,17 +153,24 @@ describe('favorites locations endpoint', () => {
   })
 
   test('should not delete a favorite location when a wrong api_key is sent', () => {
-    User.create({
+    return User.create({
       email: 'test@test.com',
       password: 'password',
-      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt'
+      api_key: 'MZDhD88Ut_C8F_dZxMpH_AJ8cpYlLcdt',
+      locations: [
+        {
+          city: 'Paris'
+        },
+        {
+          city: 'Kigali'
+        },
+        {
+          city: 'Nairobi'
+        },
+      ]
     })
     .then(user => {
-      Location.create({
-        city: 'Paris',
-        userId: user.id
-      })
-      request(app)
+      return request(app)
       .delete('/api/v1/favorites?location=Paris')
       .send({
         api_key: 'MZDhxxxt_C8F_dZxMpH_AJ8cpYlLcdt'
@@ -150,7 +178,8 @@ describe('favorites locations endpoint', () => {
       .then(response => {
         expect(response.statusCode).toBe(401)
         expect(Object.keys(response.body)).toContain('error')
-        expect(Object.keys(response.body.error)).toEqual('Api_key submit is incorrect');
+        console.log(response.body)
+        expect(response.body.error).toEqual('Api_key submit is incorrect');
       })
     }) 
   })
